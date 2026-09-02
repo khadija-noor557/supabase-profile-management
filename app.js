76d8a050a3e8bbffd8674f26305ec375ec205c94
noorkhadija557 <noorkhadija557@gmail.com>
2026-09-02 15:55:52 +0500

    working

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-const emptyState = document.querySelector("#emptyState");
-const filledState = document.querySelector("#filledState");
+// const emptyState = document.querySelector("#emptyState");
+// const filledState = document.querySelector("#filledState");
 
-const profileInput = document.querySelector("#profileInput");
-const profileInput2 = document.querySelector("#profileInput2");
+// const profileInput = document.querySelector("#profileInput");
+// const profileInput2 = document.querySelector("#profileInput2");
 
-const profilePreview = document.querySelector("#profilePreview");
+// const profilePreview = document.querySelector("#profilePreview");
 
 
 // Show filled UI
-function showFilledState() {
+// function showFilledState() {
 
-    emptyState.classList.add("d-none");
-    filledState.classList.remove("d-none");
+//     emptyState.classList.add("d-none");
+//     filledState.classList.remove("d-none");
 
-}
+// }
 
 
 // When user clicks any input
-document.querySelectorAll(".profile-input").forEach(input => {
+// document.querySelectorAll(".profile-input").forEach(input => {
 
-    input.addEventListener("focus", () => {
+//     input.addEventListener("focus", () => {
 
-        showFilledState();
+//         showFilledState();
 
-    });
+//     });
 
-});
+// });
 
 
 // When user selects profile image
-profileInput.addEventListener("change", function () {
+// profileInput.addEventListener("change", function () {
 
-    if (this.files && this.files[0]) {
+//     if (this.files && this.files[0]) {
 
-        const imageURL = URL.createObjectURL(this.files[0]);
+//         const imageURL = URL.createObjectURL(this.files[0]);
 
-        profilePreview.src = imageURL;
+//         profilePreview.src = imageURL;
 
-        showFilledState();
+//         showFilledState();
 
-    }
+//     }
 
-});
+// });
 
 
 // Change image again from filled state
-profileInput2.addEventListener("change", function () {
+// profileInput2.addEventListener("change", function () {
 
-    if (this.files && this.files[0]) {
+//     if (this.files && this.files[0]) {
 
-        const imageURL = URL.createObjectURL(this.files[0]);
+//         const imageURL = URL.createObjectURL(this.files[0]);
 
-        profilePreview.src = imageURL;
+//         profilePreview.src = imageURL;
 
-    }
+//     }
 
-});
+// });
